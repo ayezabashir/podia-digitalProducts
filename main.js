2789a5b5141bd08c45ffd436ff923b64cb03e28d
Ayeza <ayezabashir442@gmail.com>
2024-04-08 00:55:14 +0500

    navbar

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 const headerDropdown = document.querySelectorAll(".list-item");
 const subNavs = document.querySelectorAll('.subnav');
 
+const hideAllSubnavs = () => {
+    subNavs.forEach(subnav => {
+        subnav.classList.remove('show')
+    })
+}
+
 headerDropdown.forEach(item => {
     const btn = item.querySelector('.secondary-btn');
     const subNav = item.querySelector('.subnav');
     btn.addEventListener('click', () => {
-        if (subNav.style.display == 'flex') {
-            subNav.style.display = 'none'
-        } else {
-            subNav.style.display = 'flex'
-        }
+        hideAllSubnavs();
+        subNav.classList.toggle('show')
     })
+})
+
+document.body.addEventListener('click', (event) => {
+    if (!event.target.closest('.list-item')) {
+        hideAllSubnavs()
+    }
 })
