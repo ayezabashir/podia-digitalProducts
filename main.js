5de8d998ea8390c9fd755dc43fe49624989115cb
Ayeza <ayezabashir442@gmail.com>
2024-05-17 22:02:28 +0500

    footer

--- main.js
+++ main.js
@@ -6,6 +6,13 @@ const mbNav1 = document.querySelector('.mbNav1');
 const mbNav2 = document.querySelector('.mbNav2');
 const mbNav3 = document.querySelector('.mbNav3');
 const mbNav4 = document.querySelector('.mbNav4');
+const footerBtn1 = document.getElementById('footerBtn1');
+const footerBtn2 = document.getElementById('footerBtn2');
+const footerBtn3 = document.getElementById('footerBtn3');
+const footerBtn4 = document.getElementById('footerBtn4');
+const footerBtn5 = document.getElementById('footerBtn5');
+const footerBtn6 = document.getElementById('footerBtn6');
+const footerBtn7 = document.getElementById('footerBtn7');
 
 
 dropdownNavs.forEach((dropdownNav) => {
@@ -53,3 +60,38 @@ mbNav4.addEventListener('click', () => {
     const mbnavItem = document.querySelector('.subnav4');
     mbnavItem.classList.toggle('hide');
 })
+footerBtn1.addEventListener('click', () => {
+    const listItem = document.getElementById('footerItem1');
+    listItem.classList.toggle('hide')
+})
+
+footerBtn2.addEventListener('click', () => {
+    const listItem = document.getElementById('footerItem2');
+    listItem.classList.toggle('hide')
+})
+
+footerBtn3.addEventListener('click', () => {
+    const listItem = document.getElementById('footerItem3');
+    listItem.classList.toggle('hide')
+})
+
+footerBtn4.addEventListener('click', () => {
+    const listItem = document.getElementById('footerItem4');
+    listItem.classList.toggle('hide')
+})
+
+footerBtn5.addEventListener('click', () => {
+    const listItem = document.getElementById('footerItem5');
+    listItem.classList.toggle('hide')
+})
+
+footerBtn6.addEventListener('click', () => {
+    const listItem = document.getElementById('footerItem6');
+    listItem.classList.toggle('hide')
+})
+
+footerBtn7.addEventListener('click', () => {
+    const listItem = document.getElementById('footerItem7');
+    listItem.classList.toggle('hide')
+})
+
